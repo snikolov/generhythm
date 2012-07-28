d3e50089a22041ccdea1b9dcddeba937ab74c89d
Stanislav Nikolov <snikolov@twitter.com>
2012-07-28 00:20:38 -0400

    Comparing interarrival time pairs of beats to those of noise

--- rhythm.m
+++ rhythm.m
@@ -1,6 +1,6 @@
 function rhythm
 
-x = wavread('beat3.wav');
+x = wavread('beat6.wav');
 x = x(:,1);
 size(x)
 x = downsample(x, 100);
@@ -9,12 +9,16 @@ plot(x)
 y = abs(x) > 0.1;
 indices = 1:numel(y);
 event_indices = indices(y);
-plot(event_indices)
 
 % Distribution of 1st order interarrivals.
-for k = 1:100
-  [intarr12_1, intarr12_2] = intarrkm(event_indices, k, 5);
-  scatter(intarr12_1, intarr12_2, 'k', 'filled', 'SizeData', 5);
+rand_event_indices = indices(rand(numel(indices),1) < 0.05);
+for k = 1:1000
+  [intarr_1, intarr_2] = intarrkm(event_indices, k, k);
+  [intarr_1_r, intarr_2_r] = intarrkm(rand_event_indices, k, k);
+  subplot(121)
+  scatter(intarr_1, intarr_2, 'k', 'filled', 'SizeData', 5);
+  subplot(122)
+  scatter(intarr_1_r, intarr_2_r, 'r', 'filled', 'SizeData', 5);
   % hist(intarrk(event_indices, k), 80)
   title(k)
   pause(0.1)
