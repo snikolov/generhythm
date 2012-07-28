25cebb6ddf8caa4bde777b8269d82d979e1180ef
Stanislav Nikolov <snikolov@twitter.com>
2012-07-28 01:39:28 -0400

    getting rid of consecutive events

--- rhythm.m
+++ rhythm.m
@@ -27,13 +27,13 @@ numel(y)
 w = 10000;
 indices_w = 1:w;
 rand_y = rand(size(y)) < 0.05;
-for i = 1:1000:(numel(y) - w)
+for i = 1:100:(numel(y) - w)
   y_w = y(i:i+w-1);
   rand_y_w = rand_y(i:i+w-1);
   event_indices_w = indices_w(y_w);
   rand_event_indices_w = indices_w(rand_y_w);
-  [intarr_1, intarr_2] = intarrkm(event_indices_w, 5, 5);
-  [intarr_1_r, intarr_2_r] = intarrkm(rand_event_indices_w, 5, 5);
+  [intarr_1, intarr_2] = intarrkm(event_indices_w, 6, 6);
+  [intarr_1_r, intarr_2_r] = intarrkm(rand_event_indices_w, 6, 6);
  
   subplot(121)
   A = points2image(intarr_1, intarr_2);
@@ -41,6 +41,7 @@ for i = 1:1000:(numel(y) - w)
   A = imresize(A, 200/s);
   A = conv2(A, fspecial('gaussian', 20, 5), 'same');
   imagesc(A)
+  %axis equal
   %scatter(intarr_1, intarr_2, 'k', 'filled', 'SizeData', 5);
   
   subplot(122)
@@ -49,6 +50,7 @@ for i = 1:1000:(numel(y) - w)
   A = imresize(A, 200/s);
   A = conv2(A, fspecial('gaussian', 20, 5), 'same');
   imagesc(A)
+  %axis equal
   %scatter(intarr_1_r, intarr_2_r, 'r', 'filled', 'SizeData', 5);
   title(i)
   pause(0.01);
@@ -68,3 +70,6 @@ intarr(intarr < 100) = [];
 function [intarrk,intarrm] = intarrkm(s, k, m)
 intarrk = s(k+1:end-m) - s(1:end-m-k);
 intarrm = s(k+m+1:end) - s(k+1:end-m);
+bad_ind = (intarrk < 50 | intarrm < 50);
+intarrk(bad_ind) = [];
+intarrm(bad_ind) = [];
